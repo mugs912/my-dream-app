877fb6ae8e165c296531435ee171a31e6acbac1a
admin <mugdhavdeshmukh1998@gmail.com>
2020-03-05 10:37:24 +0530

    worked on event handling and how to dynamically change web pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
-  WelcomeMsg='I am ISSC student';
-  student={
-    name:'Mugdha',
-    age:21
+  WelcomeMsg = 'I am ISSC student';
+  student = {
+    name: 'Mugdha',
+    age: 21
   };
-  students=['Rohan','Satish','Shreyas','Ajay','Akash','Vikas'];
+  students = ['Rohan', 'Satish', 'Shreyas', 'Ajay', 'Akash', 'Vikas'];
+  teachers = [
+    {
+      name: 'Bedekar mam',
+      subject: 'Numerical Analysis'
+    },
+    {
+      name: 'Tondon Sir',
+      subject: 'PPL2'
+    },
+    {
+      name: 'Sonali Mam',
+      subject: 'Soft Skills'
+    }
+  ];
+  day = 'Tuesday';
+  showWhiteText = false;
+  showBoldText = false;
+  reactLogoLink = './assets/reactLogo.png';
+
+  sayHello(){
+    //alert('Hello I am Mugdha');
+    this.showWhiteText = !this.showWhiteText;
+    this.showBoldText = !this.showBoldText;
+  }
 }
